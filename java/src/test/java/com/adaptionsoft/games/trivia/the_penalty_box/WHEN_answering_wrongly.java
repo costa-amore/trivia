8f2588520108db06559e185c9d0c35c87ea9fa79
olivier <olivier.costa@aegisoft.be>
2020-06-20 22:55:58 +0200

    Adding : tests to guard the penaltybox behavior

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/the_penalty_box/WHEN_answering_wrongly.java
@@ -0,0 +1,24 @@
+package com.adaptionsoft.games.trivia.the_penalty_box;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class WHEN_answering_wrongly {
+    @Disabled("todo")
+    @Test
+    void THEN_you_go_to_the_penalty_box(){
+
+    }
+
+    @Disabled("todo")
+    @Test
+    void THEN_you_cant_answer_a_question(){
+
+    }
+
+    @Disabled("todo")
+    @Test
+    void THEN_you_get_no_gold(){
+
+    }
+}
