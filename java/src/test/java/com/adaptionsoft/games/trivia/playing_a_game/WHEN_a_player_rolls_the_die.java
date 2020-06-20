8e2f3876e84b7fcaa8d6c68e28d2b107b70fe141
olivier <olivier.costa@aegisoft.be>
2020-06-20 21:36:38 +0200

    Refactored : make the testexamples & results more precise

--- java/src/test/java/com/adaptionsoft/games/trivia/playing_a_game/WHEN_a_player_rolls_the_die.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/playing_a_game/WHEN_a_player_rolls_the_die.java
@@ -16,14 +16,24 @@ public class WHEN_a_player_rolls_the_die extends TestsThatAssertViaTheLog {
     private Game game;
     private ByteArrayOutputStream log;
 
-    static Stream<Arguments> examples(){
+    static Stream<Arguments> locationExamples(){
         return Stream.of(
-                Arguments.of(1, 1, "Science")
-                ,Arguments.of(2, 2, "Sports")
-                ,Arguments.of(3, 3, "Rock")
-                ,Arguments.of(4, 4, "Pop")
-                ,Arguments.of(5, 5, "Science")
-                ,Arguments.of(6, 6, "Sports")
+                Arguments.of(1, 1)
+                ,Arguments.of(2, 2)
+                ,Arguments.of(3, 3)
+                ,Arguments.of(4, 4)
+                ,Arguments.of(5, 5)
+                ,Arguments.of(6, 6)
+        );
+    }
+    static Stream<Arguments> questionCategoryExamples(){
+        return Stream.of(
+                Arguments.of(1, "Science")
+                ,Arguments.of(2, "Sports")
+                ,Arguments.of(3, "Rock")
+                ,Arguments.of(4, "Pop")
+                ,Arguments.of(5, "Science")
+                ,Arguments.of(6, "Sports")
         );
     }
 
@@ -38,8 +48,8 @@ public class WHEN_a_player_rolls_the_die extends TestsThatAssertViaTheLog {
     }
 
     @ParameterizedTest
-    @MethodSource("examples")
-    void THEN_they_move_that_amount_on_the_board(int rollResult, int expectedLocation, String expectedQuestionCategory) {
+    @MethodSource("locationExamples")
+    void THEN_they_move_that_amount_on_the_board(int rollResult, int expectedLocation) {
 
         game.roll(rollResult);
 
@@ -47,8 +57,8 @@ public class WHEN_a_player_rolls_the_die extends TestsThatAssertViaTheLog {
     }
 
     @ParameterizedTest
-    @MethodSource("examples")
-    void THEN_they_get_a_question_from_the_expected_category(int rollResult, int expectedLocation, String expectedQuestionCategory) {
+    @MethodSource("questionCategoryExamples")
+    void THEN_they_get_a_question_from_the_expected_category(int rollResult, String expectedQuestionCategory) {
 
         game.roll(rollResult);
 
