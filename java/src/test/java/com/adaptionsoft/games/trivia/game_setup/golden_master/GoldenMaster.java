8738a40885b063178c6c502ba2d40640db415884
olivier <olivier.costa@aegisoft.be>
2020-06-21 17:29:49 +0200

    Fixed : bug in golden master test - stopped generating the golden master on each test run - make sure any old files get removed (else they seem to keep the new ones from getting saved) - plus some clean up

--- java/src/test/java/com/adaptionsoft/games/trivia/game_setup/golden_master/GoldenMaster.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/game_setup/golden_master/GoldenMaster.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,15 +15,15 @@ public class GoldenMaster {
 
     @Test
     void WHEN_running_the_the_same_game_THEN_the_log_should_be_the_same() throws IOException {
-        File log = RunTestAndCaptureLogIn(addPathTo("RunLog.txt"));
-        File master = new File(addPathTo("Golden_master.txt"));
+        File log = RunTestAndCaptureLogIn("RunLog.txt");
+        File master = new File("Golden_master.txt");
 
         assertThat(log).hasSameTextualContentAs(master);
     }
 
 //    only run this 'test' to generate the golden master")
-    @Test
-//    @SuppressWarnings("unused")
+//    @Test
+    @SuppressWarnings("unused")
     void Generate_golden_master() throws IOException {
         RunTestAndCaptureLogIn("Golden_master.txt");
     }
@@ -36,16 +37,17 @@ public class GoldenMaster {
 
         String pathOfThisJavaClass = GoldenMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         String relativePath = RELATIVE_PATH_OF_THE_FOLDER_STRUCTURE_WHERE_THIS_CLASS_IS_STORED + fileName;
-        return pathOfThisJavaClass.substring(0, pathOfThisJavaClass.length()- LAST_PART_OF_THE_PATH_OF_THE_CLASS_WHICH_I_DONT_GET).concat(relativePath);
+        return pathOfThisJavaClass.substring(0, pathOfThisJavaClass.length() - LAST_PART_OF_THE_PATH_OF_THE_CLASS_WHICH_I_DONT_GET).concat(relativePath);
     }
 
-    private File RunTestAndCaptureLogIn(String logFileNameAndPath) throws IOException {
-        final File logFile = new File(addPathTo(logFileNameAndPath));
+    private File RunTestAndCaptureLogIn(String logFileName) throws IOException {
+        final File logFile = new File(addPathTo(logFileName));
+        Files.deleteIfExists(logFile.toPath());
         if (!logFile.createNewFile()) return logFile;
 
         System.setOut(new PrintStream(logFile));
 
-        for (int i=0; i<100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             GameRunner.main(new String[]{String.valueOf(i)});
         }
 
