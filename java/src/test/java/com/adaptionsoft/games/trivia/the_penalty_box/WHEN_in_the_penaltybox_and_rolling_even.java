0e35218351eb0ec761dc43b9e6c4343bc707c0a9
olivier <olivier.costa@aegisoft.be>
2020-06-21 17:06:15 +0200

    Added : tests to guard the 'stay in the box' behavior

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/the_penalty_box/WHEN_in_the_penaltybox_and_rolling_even.java
@@ -0,0 +1,40 @@
+package com.adaptionsoft.games.trivia.the_penalty_box;
+
+import com.adaptionsoft.games.trivia.GameMother;
+import com.adaptionsoft.games.trivia.TestsThatAssertViaTheLog;
+import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WHEN_in_the_penaltybox_and_rolling_even extends TestsThatAssertViaTheLog {
+    public static final int PLAYER1_1st_ROLL = 1;
+    private Game game;
+
+
+    @Override
+    protected void arrange() {
+        game = GameMother
+                .createGameWithTwoPlayers()
+                .with1stPlayerInThePenaltyBoxAfterRolling(PLAYER1_1st_ROLL)
+                .with2ndPlayerAnsweringCorrecly()
+                .spawn();
+    }
+
+    private static Stream<Arguments> evenRolls() {
+
+        return Stream.of(Arguments.of(2), Arguments.of(4), Arguments.of(6));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("evenRolls")
+    void THEN_you_stay_in(int evenRollResult) {
+        game.roll(evenRollResult);
+        assertThat(logInterceptor.readLog()).contains("player1 is not getting out of the penalty box");
+    }
+}
