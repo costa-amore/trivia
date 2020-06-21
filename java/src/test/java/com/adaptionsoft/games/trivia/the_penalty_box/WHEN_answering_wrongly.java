58601b1a99cf4ee1c0e31fdd5b0cab72f3ecfec1
olivier <olivier.costa@aegisoft.be>
2020-06-21 16:15:12 +0200

    Added : tests to guard 'entering the penaltybox' behavior

--- java/src/test/java/com/adaptionsoft/games/trivia/the_penalty_box/WHEN_answering_wrongly.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/the_penalty_box/WHEN_answering_wrongly.java
@@ -1,24 +1,45 @@
 package com.adaptionsoft.games.trivia.the_penalty_box;
 
+import com.adaptionsoft.games.trivia.GameMother;
+import com.adaptionsoft.games.trivia.TestsThatAssertViaTheLog;
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class WHEN_answering_wrongly {
-    @Disabled("todo")
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WHEN_answering_wrongly extends TestsThatAssertViaTheLog {
+    private Game game;
+
+    @Override
+    protected void arrange() {
+        game = GameMother
+                .createGameWithTwoPlayers()
+                .spawn();
+        game.roll(1);
+    }
+
+    @Override
+    protected void act() {
+        game.wrongAnswer();
+        game.roll(1);
+    }
+
     @Test
     void THEN_you_go_to_the_penalty_box(){
-
+        assertThat(logInterceptor.readLog()).contains("player1 was sent to the penalty box");
     }
 
-    @Disabled("todo")
     @Test
     void THEN_you_cant_answer_a_question(){
-
+        assertThat(logInterceptor.readLog()).contains(
+                "Question was incorrectly answered\r\n" +
+                "player1 was sent to the penalty box\r\n" +
+                "player2 is the current player");
     }
 
-    @Disabled("todo")
     @Test
     void THEN_you_get_no_gold(){
-
+        assertThat(logInterceptor.readLog()).doesNotContain("player1 now has 1 Gold Coins.");
     }
 }
